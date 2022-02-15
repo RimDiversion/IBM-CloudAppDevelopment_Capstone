e423174ab569e4060f0c2bac8016746c0eaff8b9
RimDiversion <danchristensen9@gmail.com>
2022-02-15 13:12:52 -0500

    week 1 complete

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "bestcars-project.us-south.cf.appdomain.cloud"
+    ]
 
 
 # Application definition
